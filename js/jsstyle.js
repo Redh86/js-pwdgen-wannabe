09e6f3ce360f41d374e10861d8ca23bfd5fd53f3
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-02-18 15:21:46 +0100

    completo
    non so bene come inventariare il codice js

---
+++ js/jsstyle.js
@@ -0,0 +1,8 @@
+const numb = 19;
+
+var nome = prompt ("Inserire nome")
+var cognome = prompt ("Inserire cognome")
+var colorepref = prompt ("Inserire colore preferito")
+
+
+document.getElementById("tot").innerHTML = nome + cognome + colorepref + numb;
